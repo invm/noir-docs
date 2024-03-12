1c548b3e9724554d4a9608205b28ebf501bf122a
Michael Ionov <imichaelionov@gmail.com>
2024-03-12 08:33:44 +0200

    feat: add product hunt link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,40 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <img src="/img/icon.png" alt="Noir logo" width={150} height={'auto'} />
+        <img src="/img/icon.png" alt="Noir logo" width={150} height={"auto"} />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/getting-started">
+          <Link
+            className="button button--warning button--lg"
+            to="/getting-started"
+          >
             Learn more
           </Link>
-          <Link className="button button--warning button--lg" to="https://github.com/invm/noir">
+          <Link
+            className="button gh-color button--lg"
+            to="https://github.com/invm/noir"
+          >
             Star on GitHub
+          </Link>
+          <Link
+            className="button ph-color button--lg"
+            to="https://www.producthunt.com/posts/noir-3"
+          >
+            Upvote on ProductHunt
           </Link>
         </div>
       </div>
@@ -39,17 +51,29 @@ export default function Home() {
       </main>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding: '32px 0 32px 0',
-        }}>
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "32px 0 32px 0",
+        }}
+      >
         <Heading as="h2" className="text--warning">
           See Noir in action
         </Heading>
-        <div style={{ display: 'flex', justifyContent: 'center', padding: '32px 16px 32px 16px' }}>
-          <video style={{ width: 800, maxWidth: '100%' }} autoPlay={false} height="auto" controls>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "32px 16px 32px 16px",
+          }}
+        >
+          <video
+            style={{ width: 800, maxWidth: "100%" }}
+            autoPlay={false}
+            height="auto"
+            controls
+          >
             <source src="/noir.mp4" type="video/mp4" />
           </video>
         </div>
